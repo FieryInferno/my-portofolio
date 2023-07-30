7285711f3b65c3b78b1a4b09d84c6a5b6c9cc192
FieryInferno <bagassetia271@gmail.com>
2023-07-30 16:55:54 +0700

    feat:services

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from '@/components/navbar';
 import Hero from '@/components/hero';
+import Services from '@/components/services';
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <Hero />
+            <Services />
         </>
     )
 }
