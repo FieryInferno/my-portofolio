346ccc6f80cf7b68f7b1404fab02565f9983ee53
FieryInferno <bagassetia271@gmail.com>
2023-07-30 11:12:51 +0700

    feat:bootstrap animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef } from "react";
 import Image from "next/image";
 import html from "../../public/html.svg";
+import bootstrap from "../../public/bootstrap.svg";
 
 export default () => {
     const circle = useRef<HTMLDivElement>(null);
@@ -25,16 +26,21 @@ export default () => {
         <div className="relative h-screen">
             <div className="bg-[#EAEBFD] w-[40rem] h-[40rem] rounded-full absolute -top-[33rem] -left-[83%]"></div>
             <div className="bg-[#AEBBFF] w-4 h-4 rounded-full absolute top-12 left-12 transition-all duration-1000" ref={circle}></div>
-            <div className="relative">
-                <div className="mx-4">
+            <div className="mx-4">
+                <div className="relative">
                     <div className="text-[#5044EB] text-2xl mt-36 font-medium">
                         Hello! I am M. Bagas Setia
                     </div>
                     <div className="my-8 text-4xl font-bold">Programmer</div>
                     <div className="text-[#636D78] text-xl mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae urna risus. Mauris consequat tortor elit, a blandit arcu consequat eu. Sed id sollicitudin risus. Nullam in aliquet massa.</div>
                     <a className="bg-[#5044eb] text-white py-4 px-8 font-medium rounded-2xl relative inline-block overflow-hidden z-10 cursor-pointer before:absolute before:h-24 before:w-56 before:bg-black before:rounded-[50%]  before:-left-4  before:-top-24  before:-z-10 hover:before:top-[-1rem] hover:before:transition-all hover:before:duration-300">DOWNLOAD CV</a>
-                    <div className="absolute -bottom-12 right-8 bg-white rounded-[50%] w-20 h-20 flex justify-center shadow-lg hm4-hero-right">
+                    <div className="icon-wrapper w-20 h-20 right-8">
                         <Image src={html} alt="HTML" />
+                    </div>
+                </div>
+                <div className="h-screen">
+                    <div className="icon-wrapper w-24 h-24 -right-12">
+                        <Image src={bootstrap} alt="boostrap" />
                     </div>
                 </div>
             </div>
