957b8d5d59bf79be7a7bb0d63ff993003cdf3ecc
FieryInferno <bagassetia271@gmail.com>
2023-07-31 15:45:07 +0700

    feat:animation navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { config } from '@fortawesome/fontawesome-svg-core'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Sidebar from '@/components/sidebar';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
@@ -13,9 +13,38 @@ export default function navbar() {
     const [isOpen, setIsOpen] = useState(false);
     const handleClick = (e:any) => setIsOpen(!isOpen);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const navbar = document.querySelector('#navbar');
+            const distanceFromTop = navbar?.getBoundingClientRect().top;
+
+            if (distanceFromTop && distanceFromTop <= -300) {
+                navbar?.classList.replace('relative', 'fixed');
+                navbar?.classList.add('top-0');
+                navbar?.classList.add('left-0');
+                navbar?.classList.add('right-0');
+                navbar?.classList.add('sticky-on');
+            } else {
+                if (window.scrollY === 0) {
+                    navbar?.classList.replace('fixed', 'relative');
+                    navbar?.classList.remove('top-0');
+                    navbar?.classList.remove('left-0');
+                    navbar?.classList.remove('right-0');
+                    navbar?.classList.remove('sticky-on');
+                }
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+
     return (
         <>
-            <div className="flex p-4 justify-between items-center relative z-10 bg-white">
+            <div className="flex p-4 justify-between items-center relative z-10 bg-white" id="navbar">
                 <div className='font-bold'>My Portofolio</div>
                 <div className="bg-black w-10 h-10 p-2 rounded-md text-center cursor-pointer hover:bg-[#5044EB]" onClick={handleClick}>
                     <FontAwesomeIcon icon={faBars} style={{color: "#fcfcfc",}} />
