9d93eec796d970b6f51e6cfd00bc25e45d10ddf2
FieryInferno <bagassetia271@gmail.com>
2023-08-01 15:01:50 +0700

    fix:about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,8 @@
-export default () => {
-    return (
-        <></>
-    );
-}
+export default () => <div className="pt-12 bg-[#F7F5F2] px-3">
+    <div className="h-96">Untuk foto nanti</div>
+    <div className="text-4xl font-bold">M. Bagas Setia</div>
+    <div className="my-8 text-[#5044EB]">Programmer</div>
+    <div className="font-medium">
+        Continually utilize 24/365 bandwidth before real-time interfaces grow team core <span className="text-[#636D78]">competencies with pandemic commerce. Objectively initiate pandemic users with deliver bricks clicks meta services for bricks and clicks innovation streamline front end aradigms expedite granular human capital rather than intuitive testing procedures</span>
+    </div>
+</div>;
