638c02cbc61e613a1829549b112be2279e30d0c4
FieryInferno <bagassetia271@gmail.com>
2023-08-01 14:33:15 +0700

    eat:ticker section

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -4,7 +4,7 @@ import webDesignAndLogo from '../../public/web-design-and-logo.svg';
 import technologySolution from '../../public/technology-solution.svg';
 import appDevelopment from '../../public/app-development.svg';
 import ServiceItem from '@/components/service_item';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faPhone, faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import InformationItem from './InformationItem';
 
@@ -48,26 +48,33 @@ const information = [
     },
 ]
 
-export default () => {
-    return (
-        <div className="services">
-            <div className="pt-16 text-center text-white underline underline-offset-8 font-medium text-xl">What's Going On</div>
-            <div className="text-center text-white text-3xl font-bold mt-8">My Service Area</div>
-            <div className='my-20'>
-                {services.map(({image, title}, index) => <ServiceItem key={`serviceItem${index}`} image={image} title={title} />)}
-            </div>
-            <div className='p-12 bg-[#4752d8c4] border border-[#6770F6] rounded-3xl shadow-[0_13px_42px_#3B4BBF] service-card'>
-                <span className='text-[#5044EB] bg-white p-6 rounded-[50%] text-2xl inline-block mb-8'>
-                    <FontAwesomeIcon icon={faPhone} className='service-icon' />
-                </span>
-                <div className='text-white font-bold'>
-                    <div className='text-2xl'>+62857 2385 3284</div>
-                    <div className='uppercase'>call me directly anytime</div>
-                </div>
-            </div>
-            <div className='my-20 text-center'>
-                {information.map(({icon, count, description}, index) => <InformationItem key={`information${index}`} icon={icon} count={count} description={description} />)}
+export default () => <div className="services">
+    <div className='px-2'>
+        <div className="pt-16 text-center text-white underline underline-offset-8 font-medium text-xl">What's Going On</div>
+        <div className="text-center text-white text-3xl font-bold mt-8">My Service Area</div>
+        <div className='my-20'>
+            {services.map(({image, title}, index) => <ServiceItem key={`serviceItem${index}`} image={image} title={title} />)}
+        </div>
+        <div className='p-12 bg-[#4752d8c4] border border-[#6770F6] rounded-3xl shadow-[0_13px_42px_#3B4BBF] service-card'>
+            <span className='text-[#5044EB] bg-white p-6 rounded-[50%] text-2xl inline-block mb-8'>
+                <FontAwesomeIcon icon={faPhone} className='service-icon' />
+            </span>
+            <div className='text-white font-bold'>
+                <div className='text-2xl'>+62857 2385 3284</div>
+                <div className='uppercase'>call me directly anytime</div>
             </div>
         </div>
-    )
-}
+        <div className='py-20 text-center'>
+            {information.map(({icon, count, description}, index) => <InformationItem key={`information${index}`} icon={icon} count={count} description={description} />)}
+        </div>
+    </div>
+    <div className='ticker-section'>
+        <ul className='ur-ticker'>
+            <li className='inline-block mr-12 uppercase'>we do agency solution</li>
+            <li className='inline-block mr-12 uppercase'>what we love to work with</li>
+            <li className='inline-block mr-12 uppercase'>what we do</li>
+            <li className='inline-block mr-12 uppercase'>we love to work with</li>
+            <li className='inline-block mr-12 uppercase'>we love what we do</li>
+        </ul>
+    </div>
+</div>;
