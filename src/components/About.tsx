0fd9277b58afd0b0033ea96edc498efd0c5b60ca
FieryInferno <bagassetia271@gmail.com>
2023-08-02 22:23:18 +0700

    fix:services section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import { faEnvelope, faPhone, faUser, faLocation } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PersonalInformation from '@/components/PersonalInformation';
+import Button from "./Button";
 
 const personalInformation = [
     {
@@ -32,7 +33,9 @@ export default () => <div className="pt-12 bg-[#F7F5F2] px-3">
     <div className="font-medium">
         Continually utilize 24/365 bandwidth before real-time interfaces grow team core <span className="text-[#636D78]">competencies with pandemic commerce. Objectively initiate pandemic users with deliver bricks clicks meta services for bricks and clicks innovation streamline front end aradigms expedite granular human capital rather than intuitive testing procedures</span>
     </div>
-    <div className="mt-12 bg-white p-8 rounded-2xl">
+    <div className="my-12 bg-white p-8 rounded-2xl">
         {personalInformation.map(({icon, title, description}, index) => <PersonalInformation key={`personalInformation${index}`} icon={icon} title={title} description={description} />)}
     </div>
+    <Button>HIRE ME NOW</Button>
+    <Button type="white">DONWLOAD CV</Button>
 </div>;
---
+++ src/components/Button.tsx
@@ -0,0 +1,3 @@
+import { ReactNode } from "react";
+
+export default ({children, type = 'blue'} : {children: ReactNode, type?: string}) => <a className={`${type === 'white' ? 'bg-white text-[#5044EB] before:bg-[#5044EB] hover:text-white' : 'bg-[#5044eb] text-white before:bg-black'} py-4 px-8 font-medium rounded-2xl relative inline-block overflow-hidden cursor-pointer z-[1] before:absolute before:h-24 before:w-56 before:rounded-[50%]  before:-left-4  before:-top-24  before:-z-10 hover:before:top-[-1rem] hover:before:transition-all hover:before:duration-300`}>{children}</a>
