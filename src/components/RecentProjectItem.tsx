1cb4c130b502776aefdf6817053768721978baa4
FieryInferno <bagassetia271@gmail.com>
2023-09-16 18:28:04 +0700

    my recent project

--- src/components/RecentProjectItem.tsx
+++ src/components/RecentProjectItem.tsx
@@ -1,12 +1,19 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { StaticImport } from "next/dist/shared/lib/get-img-props"
 import Image from "next/image"
 
 export default function(props: { image: StaticImport; subtitle: string; title: string }) {
     return (
         <div className="recent-project-item">
-            <Image src={props.image} alt="project" className="rounded-lg mb-8" />
+            <div>
+                <div className="explore-btn">
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </div>
+                <Image src={props.image} alt="project" className="rounded-lg mb-8" />
+            </div>
             <div className="text-[#636D78]">{props.subtitle}</div>
-            <div className="font-bold">{props.title}</div>
+            <h1 className="font-bold text-xl">{props.title}</h1>
         </div>
     )
 }
